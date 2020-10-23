98bedae9dc0e0de1deaa4c1a952dec6935678f08
Farouk Nnajiofor <uzomaglobal@gmail.com>
2020-10-22 23:22:01 -0700

    Refactored constants to a different file

--- main.go
+++ main.go
@@ -8,28 +8,6 @@ import (
 	"strings"
 )
 
-type jokingRandomlyResponse struct {
-	Message string `json:"message"`
-}
-
-type randomNameResponse struct {
-	Name string `json:"name"`
-}
-
-type randomJokeResponse struct {
-	Type  string `json:"type"`
-	Value joke   `json:"value"`
-}
-
-type joke struct {
-	Joke string `json:"joke"`
-}
-
-const randomJokeNameConstant = "John Doe"
-const errorConstant = "Error: "
-const randomNameURL = "https://api.namefake.com/"
-const randomJokeURL = "http://api.icndb.com/jokes/random?firstName=John&lastName=Doe&limitTo=[nerdy]"
-
 func main() {
 	port := 5000
 	fmt.Println("Joking Randomly >> starting...")
